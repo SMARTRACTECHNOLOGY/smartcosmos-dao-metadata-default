080c31f6f866786be768e73a1d4e7bfe5e250ced
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-05-31 10:43:55 +0200

    Add upsert() method

--- src/main/java/net/smartcosmos/dao/metadata/impl/MetadataPersistenceService.java
+++ src/main/java/net/smartcosmos/dao/metadata/impl/MetadataPersistenceService.java
@@ -1,20 +1,22 @@
 package net.smartcosmos.dao.metadata.impl;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
-
-import javax.validation.ConstraintViolationException;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.convert.ConversionService;
-import org.springframework.stereotype.Service;
-
 import lombok.extern.slf4j.Slf4j;
 import net.smartcosmos.dao.metadata.MetadataDao;
+import net.smartcosmos.dao.metadata.domain.MetadataEntity;
 import net.smartcosmos.dao.metadata.repository.MetadataRepository;
 import net.smartcosmos.dto.metadata.MetadataResponse;
 import net.smartcosmos.dto.metadata.MetadataUpsert;
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionException;
+
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -31,8 +33,18 @@ public class MetadataPersistenceService implements MetadataDao {
     }
 
     @Override
-    public List<MetadataResponse> upsert(String accountUrn, String entityReferenceType, String referenceUrn, Collection<MetadataUpsert> metadataUpsertList) throws ConstraintViolationException {
-        return null;
+    public List<MetadataResponse> upsert(String accountUrn, Collection<MetadataUpsert> upsertMetadataCollection) throws ConstraintViolationException {
+
+        List<MetadataResponse> responseList = new ArrayList<>();
+
+        for (MetadataUpsert upsertMetadata : upsertMetadataCollection) {
+            MetadataEntity entity = conversionService.convert(upsertMetadata, MetadataEntity.class);
+            entity = persist(entity);
+
+            responseList.add(conversionService.convert(entity, MetadataResponse.class));
+        }
+
+        return responseList;
     }
 
     @Override
@@ -43,5 +55,28 @@ public class MetadataPersistenceService implements MetadataDao {
     @Override
     public Optional<MetadataResponse> findByKey(String accountUrn, String entityReferenceType, String referenceUrn, String key) {
         return null;
+    }
+
+    /**
+     * Saves an metadata entity in an {@link MetadataRepository}.
+     *
+     * @param metadataEntity the metadata entity to persist
+     * @return the persisted metadata entity
+     * @throws ConstraintViolationException if the transaction fails due to violated constraints
+     * @throws TransactionException if the transaction fails because of something else
+     */
+    @SuppressWarnings("Duplicates")
+    private MetadataEntity persist(MetadataEntity metadataEntity) throws ConstraintViolationException, TransactionException {
+        try {
+            return metadataRepository.save(metadataEntity);
+        } catch (TransactionException e) {
+            // we expect constraint violations to be the root cause for exceptions here,
+            // so we throw this particular exception back to the caller
+            if (ExceptionUtils.getRootCause(e) instanceof ConstraintViolationException) {
+                throw (ConstraintViolationException) ExceptionUtils.getRootCause(e);
+            } else {
+                throw e;
+            }
+        }
     }
 }
