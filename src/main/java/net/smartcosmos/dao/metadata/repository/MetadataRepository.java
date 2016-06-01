9752cabfe05f1f9c35ce622848f17a41f2e2d5da
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-06-01 12:45:52 +0200

    Declare findByKey repository method

--- src/main/java/net/smartcosmos/dao/metadata/repository/MetadataRepository.java
+++ src/main/java/net/smartcosmos/dao/metadata/repository/MetadataRepository.java
@@ -5,11 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface MetadataRepository extends JpaRepository<MetadataEntity, UUID> {
 
     List<MetadataEntity> findByAccountIdAndReferenceId(UUID accountId, UUID referenceId);
+
+    Optional<MetadataEntity> findByAccountIdAndEntityReferenceTypeAndReferenceIdAndKey(UUID accountId, String entityReferenceType, UUID referenceId, String key);
 
     @Transactional
     List<MetadataEntity> deleteByAccountIdAndEntityReferenceTypeAndReferenceIdAndKey(UUID accountId, String entityReferenceType, UUID referenceId, String key);
