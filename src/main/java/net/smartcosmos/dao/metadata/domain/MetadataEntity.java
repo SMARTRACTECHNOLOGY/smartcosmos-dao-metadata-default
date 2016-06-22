49a53df17d9ae1153511824b75d026745713447c
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-06-22 21:29:01 +0200

    Replace generated UUID ID by composite key

--- src/main/java/net/smartcosmos/dao/metadata/domain/MetadataEntity.java
+++ src/main/java/net/smartcosmos/dao/metadata/domain/MetadataEntity.java
@@ -1,37 +1,28 @@
 package net.smartcosmos.dao.metadata.domain;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
+import lombok.*;
 import org.hibernate.annotations.Type;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.io.Serializable;
 import java.util.UUID;
 
 @Entity(name = "metadata")
+@IdClass(MetadataId.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @AllArgsConstructor
 @Data
 @EntityListeners({ AuditingEntityListener.class })
 @Table(name = "metadata", uniqueConstraints = @UniqueConstraint(
-    columnNames = { "id", "tenantId"}) )
-public class MetadataEntity {
+    columnNames = { "ownerId", "tenantId", "dataType", "keyName"}) )
+public class MetadataEntity implements Serializable {
 
     private static final int UUID_LENGTH = 16;
     private static final int KEY_NAME_LENGTH = 255;
@@ -40,17 +31,12 @@ public class MetadataEntity {
     private static final int VALUE_LENGTH = 767;
 
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
-    @Type(type = "uuid-binary")
-    @Column(name = "id", length = UUID_LENGTH)
-    private UUID id;
-
     @NotEmpty
     @Size(max = OWNER_TYPE_LENGTH)
     @Column(name = "ownerType", length = OWNER_TYPE_LENGTH, nullable = false, updatable = false)
     private String ownerType;
 
+    @Id
     @NotNull
     @Type(type = "uuid-binary")
     @Column(name = "ownerId", length = UUID_LENGTH, nullable = false, updatable = false)
@@ -61,16 +47,17 @@ public class MetadataEntity {
     @Column(name = "dataType", length = DATA_TYPE_LENGTH, nullable = false, updatable = true)
     private String dataType;
 
+    @Id
     @NotEmpty
     @Size(max = KEY_NAME_LENGTH)
     @Column(name = "keyName", length = KEY_NAME_LENGTH, nullable = false, updatable = false)
     private String keyName;
 
-    //@NotEmpty
     @Size(max = VALUE_LENGTH)
     @Column(name = "value", length = VALUE_LENGTH, nullable = true, updatable = true)
     private String value;
 
+    @Id
     @NotNull
     @Type(type = "uuid-binary")
     @Column(name = "tenantId", length = UUID_LENGTH, nullable = false, updatable = false)
