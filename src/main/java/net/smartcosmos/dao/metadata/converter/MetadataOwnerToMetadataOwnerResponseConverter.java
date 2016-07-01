8b8b50a8f731c36d7914d2acf093a7f9343e3958
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-07-01 15:13:48 +0200

    Add converters for metadata owner

---
+++ src/main/java/net/smartcosmos/dao/metadata/converter/MetadataOwnerToMetadataOwnerResponseConverter.java
@@ -0,0 +1,31 @@
+package net.smartcosmos.dao.metadata.converter;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.format.FormatterRegistrar;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.stereotype.Component;
+
+import net.smartcosmos.dao.metadata.domain.MetadataOwner;
+import net.smartcosmos.dao.metadata.util.UuidUtil;
+import net.smartcosmos.dto.metadata.MetadataOwnerResponse;
+
+@Component
+public class MetadataOwnerToMetadataOwnerResponseConverter
+    implements Converter<MetadataOwner, MetadataOwnerResponse>, FormatterRegistrar {
+
+    @Override
+    public MetadataOwnerResponse convert(MetadataOwner entity) {
+
+        return MetadataOwnerResponse.builder()
+            .ownerType(entity.getOwnerType())
+            .ownerUrn(UuidUtil.getThingUrnFromUuid(entity.getOwnerId()))
+            .tenantUrn(UuidUtil.getTenantUrnFromUuid(entity.getTenantId()))
+            .build();
+    }
+
+    @Override
+    public void registerFormatters(FormatterRegistry registry) {
+        registry.addConverter(this);
+    }
+}
+
